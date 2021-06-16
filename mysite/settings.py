317f6b6c8399c9ccdd52585f31f1093b96a9a759
Middleway13 <middlewayonlinemedia@gmail.com>
2021-06-16 13:04:11 -0400

    Changed the HTML for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog.apps.BlogConfig',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
